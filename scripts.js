d12fc33c99f84378b531b660940532169d3650af
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-10-11 12:49:26 -0300

    update for task 4

--- scripts.js
+++ scripts.js
@@ -144,3 +144,71 @@ function latestVideos() {
     })
 }
 latestVideos()
+
+/*
+ * Replace static quotes by dynamic loading
+ */
+function cards(q = '', topic = 'all', sort = 'most_popular') {
+    $('.loader').show()
+    $.ajax({
+        type: 'GET',
+        url: 'https://smileschool-api.hbtn.info/courses',
+        data: {
+            q: q,
+            topic: topic,
+            sort: sort,
+        },
+        error: function(error) {
+            alert(error)
+        },
+        success: function(response) {
+            if (response.courses.length === 1) {
+                let videos = document.querySelector('#videos')
+                videos.innerHTML = `1 video`
+            } else {
+                let videos = document.querySelector('#videos')
+                videos.innerHTML = `${response.courses.length} videos`
+            }
+            $('#cards').empty()
+            response.courses.forEach(function(element) {
+                $('#cards').append(
+                    `<div class="col-sm-12 col-md-4 col-lg-3">
+                        <div class="card mb-4">
+                            <img src="${element.thumb_url}" alt="" class="card-img-top">
+                            <div class="card-img-overlay d-flex justify-content-center mt-5">
+                                <img src="images/play.png" width="60" height="60">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">${element.title}</h5>
+                                <p class="card-text paragraph-card">${element['sub-title']}</p>
+                                <div class="row">
+                                    <div class="col-3">
+                                        <img src="${element.author_pic_url}" alt="" class="image" width="60" height="60">
+                                    </div>
+                                    <div class="col-6">
+                                        <h3 class="ml-4 mt-3 text-center">${element.author}</h3>
+                                    </div>
+                                </div>
+                                <div class="row d-flex justify-content-around">
+                                    <div class="ml-3 mt-1 col-6" id=''></div>
+                                    <div class="col-6">
+                                        <h3 class="mx-2 mt-2 text-end">${element.duration}</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`)
+
+                for (let i = 1; i <= 5; i++) {
+                    if (i <= element.star) {
+                        $('#stars').append('<img src="images/star_on.png" alt="" width="15" height="15">')
+                    } else {
+                        $('#stars').append('<img src="images/star_off.png" alt="" width="15" height="15">')
+                    }
+                }
+            })
+            $('.loader').hide()
+        }
+    })
+}
+cards()
